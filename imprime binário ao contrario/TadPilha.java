01b9427d7982aa92902861ddd9fd6febcc8b8a64
PhillipiMedeiros <118783606+PhillipiMedeiros@users.noreply.github.com>
2022-11-21 22:38:25 -0300

    Create TadPilha.java

---
+++ imprime binário ao contrario/TadPilha.java
@@ -0,0 +1,36 @@
+public class TadPilha
+{
+    int capacidade;
+    int topo;
+    int dados[];
+    
+    boolean cheia(){
+        return (topo == capacidade - 1);
+    }
+    
+    void empilha (int d){
+        cheia();
+        topo++;
+        dados[topo] = d;
+    }
+    
+    void imprime(){
+        for (int i=0; i<=topo; i++)
+        
+            System.out.println(dados[i]);
+            
+            
+    }
+    
+    void imprimereverso(){
+        for (int i=topo; i>=0; i--)
+        
+            System.out.println(dados[i]);
+    }        
+    
+    TadPilha(int c){
+        capacidade = c;
+        topo = -1;
+        dados = new int[c];
+    }
+}
